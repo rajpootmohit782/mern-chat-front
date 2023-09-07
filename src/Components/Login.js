562802ededa28800678e0152576ce1a17351f987
rajpootmohit782 <84062871+rajpootmohit782@users.noreply.github.com>
2023-09-07 12:15:05 +0530

    2 files modified

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,20 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useHistory
 import './myStyles.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-export default function login() {
+export default function Login() {
+  const [signupMode, setSignupMode] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    Name: '',
+    password: '',
+  });
+  const [error, setError] = useState('');
+  const [loginStatus, setLoginStatus] = useState(null); // Initialize loginStatus
+  const [signupStatus, setSignupStatus] = useState(null); // Initialize signupStatus
+
+  const history = useNavigate(); // Initialize useHistory
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      setError('');
+
+      const url = signupMode
+        ? 'https://9mv3cy-5000.csb.app/user/register'
+        : 'https://9mv3cy-5000.csb.app/user/login';
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      };
+
+      const response = await fetch(url, requestOptions);
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.message || 'Something went wrong.');
+      }
+
+      setLoading(false);
+
+      if (signupMode) {
+        setSignupStatus('success'); // Set signupStatus to 'success'
+      } else {
+        setLoginStatus('success'); // Set loginStatus to 'success'
+        localStorage.setItem('userData', JSON.stringify(responseData));
+        history('/app/Welcome'); // Navigate to the dashboard after successful login
+      }
+    } catch (err) {
+      setLoading(false);
+      setError(err.message || 'Something went wrong.');
+      console.error(err);
+    }
+  };
+
+  const toggleSignupMode = () => {
+    setSignupMode(!signupMode);
+    setError('');
+  };
+
   return (
     <div className="login-container">
-      {/*  <div className="image-container">
-      <img src={} alt="logo" className="logo" />
-    </div> */}
-
       <div className="login-box">
-        <p className="login-text"> Login Your Account</p>
-        <TextField id="standard-basic" label="Username" variant="outlined" />
-        <TextField id="standard-basic" label="Password" variant="outlined" />
-        <Button variant="outlined">Login</Button>
+        <p className="login-text">
+          {signupMode ? 'Sign Up Your Account' : 'Login Your Account'}
+        </p>
+        {signupMode && (
+          <TextField
+            name="email"
+            label="Email"
+            variant="outlined"
+            fullWidth
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        )}
+        <TextField
+          name={signupMode ? 'Name' : 'Name'}
+          label={signupMode ? 'Username' : 'Name'}
+          variant="outlined"
+          fullWidth
+          value={formData.Name}
+          onChange={handleInputChange}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          fullWidth
+          value={formData.password}
+          onChange={handleInputChange}
+        />
+
+        <Button
+          variant="outlined"
+          onClick={handleSubmit}
+          disabled={loading}
+          fullWidth
+        >
+          {loading ? 'Loading...' : signupMode ? 'Sign Up' : 'Log In'}
+        </Button>
+        <Button variant="outlined" onClick={toggleSignupMode} fullWidth>
+          {signupMode ? 'Switch to Login' : 'Switch to Signup'}
+        </Button>
+
+        {error && (
+          <p className="error-message">
+            <strong>Error:</strong> {error}
+          </p>
+        )}
+
+        {loginStatus === 'success' && <p>Login Successful!</p>}
+        {signupStatus === 'success' && <p>Signup Successful!</p>}
       </div>
     </div>
   );
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './myStyles.css';
-//import logo from '../xm.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Welcome() {
+  const navigate = useNavigate();
+  const a = JSON.parse(localStorage.getItem('userData'));
+  console.log(a);
+  React.useEffect(() => {
+    if (!a) {
+      navigate('/');
+    }
+  });
+
+  //   <img
+  //   src="your-image-source-here.png"
+  //   alt="logo"
+  //   className="welcomelogo"
+  // />
+
   return (
     <div className="welcome-container">
-      <img src={} alt={'logo'} className="welcomelogo" />
       <p>View and Text directly to the people present In the chat Rooms.</p>
     </div>
   );
